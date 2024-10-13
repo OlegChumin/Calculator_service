48e645e737a7872e5fe5ab58a20b58cabf585f3c
Oleg Chumin <tschumin.oleg@gmail.com>
2024-10-13 14:54:53 +0300

    Изменен JaegerTracingAspect

--- src/main/java/org/example/jaeger_testing/rest/CalculatorController.java
+++ src/main/java/org/example/jaeger_testing/rest/CalculatorController.java
@@ -22,44 +22,24 @@ public class CalculatorController {
     }
 
     @PostMapping("/sum")
-    public ResponseEntity<Double> sum(@RequestBody OperationRequestDTO request, HttpServletRequest httpRequest) {
-        // Логируем заголовки перед выполнением операции
-        logRelevantRequestHeaders(httpRequest);
-
+    public ResponseEntity<Double> sum(@RequestBody OperationRequestDTO request) {
         double result = calculatorService.sum(request.getA(), request.getB());
         return ResponseEntity.ok(result);
     }
 
     @PostMapping("/subtract")
-    public ResponseEntity<Double> subtract(@RequestBody OperationRequestDTO request, HttpServletRequest httpRequest) {
-        // Логируем заголовки перед выполнением операции
-        logRelevantRequestHeaders(httpRequest);
-
+    public ResponseEntity<Double> subtract(@RequestBody OperationRequestDTO request) {
         return ResponseEntity.ok(calculatorService.subtract(request.getA(), request.getB()));
     }
 
     @PostMapping("/multiply")
-    public ResponseEntity<Double> multiply(@RequestBody OperationRequestDTO request, HttpServletRequest httpRequest) {
-        // Логируем заголовки перед выполнением операции
-        logRelevantRequestHeaders(httpRequest);
-
+    public ResponseEntity<Double> multiply(@RequestBody OperationRequestDTO request) {
         return ResponseEntity.ok(calculatorService.multiply(request.getA(), request.getB()));
     }
 
     @PostMapping("/divide")
-    public ResponseEntity<Double> divide(@RequestBody OperationRequestDTO request, HttpServletRequest httpRequest) {
-        // Логируем заголовки перед выполнением операции
-        logRelevantRequestHeaders(httpRequest);
-
+    public ResponseEntity<Double> divide(@RequestBody OperationRequestDTO request) {
         return ResponseEntity.ok(calculatorService.divide(request.getA(), request.getB()));
-    }
-
-    // Метод для логирования заголовков запроса
-    private void logRelevantRequestHeaders(HttpServletRequest request) {
-        log.info("Header: jaeger_traceId = {}",
-                request.getHeader("jaeger_traceId") != null ? request.getHeader("jaeger_traceId") : "jaeger_traceId not found");
-        log.info("Header: uber-trace-id = {}",
-                request.getHeader("uber-trace-id") != null ? request.getHeader("uber-trace-id") : "uber-trace-id not found");
     }
 
 }
